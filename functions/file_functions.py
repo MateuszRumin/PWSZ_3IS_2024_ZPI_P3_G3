ff4d241494b0cacc6a8a169f1b9fc8dca67ed149
Rafał Radzik <rafal_radzik@o2.pl>
2024-03-12 17:49:53 +0100

    exporting file to stl

--- functions/file_functions.py
+++ functions/file_functions.py
@@ -82,4 +82,31 @@ class FileFunctions:
         print(mesh)
 
     def _on_export_to_stl(self, mesh):
-        print(mesh)
+        # print(mesh)
+
+        if mesh is not None:
+            try:
+                dlg = gui.FileDialog(gui.FileDialog.SAVE, "Choose file to save",
+                                     self.window.theme)
+                dlg.add_filter(".stl", "Stereolithography files (.stl)")
+                dlg.add_filter("", "All files")
+                dlg.set_on_cancel(self._on_file_dialog_cancel)
+                dlg.set_on_done(self._on_export_dialog_done)
+                self.window.show_dialog(dlg)
+            except Exception as e:
+                print("[Error] An error occurred while preparing the export:", str(e))
+        else:
+            print("[Warning] No mesh to export.")
+            # alert = gui.Dialog("No mesh to export.")
+            # self.window.show_dialog(alert)
+
+    def _on_export_dialog_done(self, filename):
+        self.window.close_dialog()
+        if filename.endswith(".stl"):
+            try:
+                o3d.io.write_triangle_mesh(filename, self.create_mesh)
+                print("[Info] Successfully exported to", filename)
+            except Exception as e:
+                print("[Error] An error occurred during the export:", str(e))
+        else:
+            print("[Warning] Invalid file format for export.")
