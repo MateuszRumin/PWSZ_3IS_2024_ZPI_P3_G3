fc3e4e97c7de59b3f21a6463c4b2da9c609ed2ef
Rafał Radzik <rafal_radzik@o2.pl>
2024-03-18 13:43:40 +0100

    Pick points and delete these points from cloud

--- functions/edit_cloud.py
+++ functions/edit_cloud.py
@@ -2,15 +2,21 @@ import numpy as np
 
 from libraries import *
 
+
 class PointEditor:
     def _edit_points(self):
         self.manual_registration()
 
-
     def manual_registration(self):
         self.source = self.cloud
         source_points = self.pick_points(self.source)
-        print(source_points)
+        print(source_points)   # [] with no. point
+        self.source = self.delete_pick_points(self.source, source_points)
+        print(f"SOURCE: ", self.source)
+
+        # Display visualization cloud without remove points
+        # source_points = self.pick_points(self.source)
+        # print(source_points)  # [] with no. point
 
     def pick_points(self, pcd):
         print(
@@ -24,5 +30,20 @@ class PointEditor:
         vis.run()  # user picks points
         vis.destroy_window()
         print("---------------------------------------------")
-        print(vis.get_cropped_geometry())
+        print(vis.get_cropped_geometry())   # PointCloud with ... points.
         return vis.get_picked_points()
+
+    def delete_pick_points(self, pcd, picked_points):
+        # List number of pick points to index
+        indices_to_delete = picked_points
+
+        # Delete picks points from org cloud
+        pcd.points = o3d.utility.Vector3dVector(
+            np.delete(np.asarray(pcd.points), indices_to_delete, axis=0)
+        )
+        if pcd.colors:
+            pcd.colors = o3d.utility.Vector3dVector(
+                np.delete(np.asarray(pcd.colors), indices_to_delete, axis=0)
+            )
+        print("Deleted picked points from the original point cloud.")
+        return pcd
